42126374ae81fec24cf52be153b992a5fcd97688
Kamol roy <kamolroy.din@gmail.com>
2017-06-18 14:31:29 +0600

    Adding data anotation for date type

--- ModelDemo/Models/ChangeProject.cs
+++ ModelDemo/Models/ChangeProject.cs
@@ -17,6 +17,7 @@ namespace ModelDemo.Models
 
         [Required]
         [Display(Name = "Date")]
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
 
         [Required]
